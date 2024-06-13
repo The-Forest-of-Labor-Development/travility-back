7d3dbd20eb6284f8d2075510906b7df5fbddaede
문태준 <pjrnoon99@gmail.com>
2024-06-13 15:40:21 +0900

    init

---
+++ src/main/java/travility_back/travility/TravilityApplication.java
@@ -0,0 +1,13 @@
+package travility_back.travility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/travility_back/travility/TravilityApplicationTests.java
@@ -0,0 +1,13 @@
+package travility_back.travility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
