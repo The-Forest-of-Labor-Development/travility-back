0aca53f3bf761487e9a546c529d3668e02d65391
heo5620 <tnqls1035@naver.com>
2024-06-13 17:40:09 +0900

    push test

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # test2
 
 # wondahee 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 !
+
+# push test
--- src/main/java/travility_back/travility/entity/Member.java
+++ src/main/java/travility_back/travility/entity/Member.java
@@ -12,4 +12,5 @@ public class Member {
     Long id;
 
     String username;
+    String password;
 }
