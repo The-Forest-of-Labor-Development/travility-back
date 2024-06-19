ca34e7c3946a4e8feae4b513805acc60181a8ba2
문태준 <pjrnoon99@gmail.com>
2024-06-19 10:14:47 +0900

    [feat] Naver 로그인 응답 인증값

---
+++ src/main/java/travility_back/travility/response/NaverResponse.java
@@ -0,0 +1,35 @@
+package travility_back.travility.response;
+
+import travility_back.travility.dto.OAuth2Response;
+
+import java.util.Map;
+
+public class NaverResponse implements OAuth2Response {
+
+    private final Map<String, Object> attribute;
+
+    // 생성자
+    public NaverResponse(Map<String, Object> attribute) {
+        this.attribute = (Map<String, Object>) attribute.get("response");
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
+}
