56b779ba7eb694c7e7224be51222aa03d4c31cdd
문태준 <pjrnoon99@gmail.com>
2024-06-30 16:43:56 +0900

    [fix] 변경없음

--- src/main/java/travility_back/travility/controller/schedule/CalendarController.java
+++ src/main/java/travility_back/travility/controller/schedule/CalendarController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import travility_back.travility.dto.CustomUserDetails;
-import travility_back.travility.entity.AccountBook;
 import travility_back.travility.service.schedule.CalendarService;
 
 import java.util.List;
