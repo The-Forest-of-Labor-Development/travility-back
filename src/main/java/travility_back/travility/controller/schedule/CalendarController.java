f010ea8177d6fd44d72194a06c6f834595281e56
daaaaaahee <167724195+daaaaaahee@users.noreply.github.com>
2024-07-04 23:20:19 +0900

    [feat]캘린더에서 그 날의 전체 지출 나타내기

--- src/main/java/travility_back/travility/controller/schedule/CalendarController.java
+++ src/main/java/travility_back/travility/controller/schedule/CalendarController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import travility_back.travility.dto.CustomUserDetails;
+import travility_back.travility.entity.Expense;
 import travility_back.travility.service.schedule.CalendarService;
 
 import java.time.LocalDate;
@@ -29,8 +30,16 @@ public class CalendarController {
         return ResponseEntity.ok(events);
     }
 
+
     @GetMapping("/schedule/{id}")
-    public Map<LocalDate, Double> getExpenseByDay(@PathVariable("id") Long id){
-        return calendarService.getExpenseByDay(id);
+    public ResponseEntity<Map<LocalDate, Double>> getExpenseByDay(@PathVariable("id") Long id) {
+        Map<LocalDate, Double> expenses = calendarService.getExpenseByDay(id);
+        return ResponseEntity.ok(expenses);
+    }
+
+    @GetMapping("/expenses/{accountBookId}")
+    public ResponseEntity<List<Expense>> getAllExpensesByAccountBookId(@PathVariable("accountBookId") Long accountBookId) {
+        List<Expense> expenses = calendarService.getAllExpensesByAccountBookId(accountBookId);
+        return ResponseEntity.ok(expenses);
     }
 }
