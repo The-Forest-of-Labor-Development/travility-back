d458c9487a575429b397a8fe27300149b8caa37f
문태준 <pjrnoon99@gmail.com>
2024-07-11 09:19:08 +0900

    [fix] 예산 - 지출 값 query

--- src/main/java/travility_back/travility/repository/BudgetRepository.java
+++ src/main/java/travility_back/travility/repository/BudgetRepository.java
@@ -1,6 +1,8 @@
 package travility_back.travility.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import travility_back.travility.entity.Budget;
 
@@ -9,4 +11,12 @@ import java.util.List;
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
     void deleteByAccountBookId(Long accountBookId);
+
+
+    // 예산 - 지출
+    @Query("SELECT SUM(CASE WHEN b.isShared = true THEN FLOOR(b.amount / ab.numberOfPeople) ELSE b.amount END) " +
+            "FROM Budget b " +
+            "JOIN b.accountBook ab " +
+            "WHERE ab.id = :accountBookId")
+    Double getTotalBudgetByAccountBookId(@Param("accountBookId") Long accountBookId);
 }
