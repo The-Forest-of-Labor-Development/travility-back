5e039680ec972598fcf997c47a4ebc2f6c1dea15
문태준 <pjrnoon99@gmail.com>
2024-07-17 15:15:36 +0900

    [fix] query 환율 적용

--- src/main/java/travility_back/travility/repository/ExpenseRepository.java
+++ src/main/java/travility_back/travility/repository/ExpenseRepository.java
@@ -48,28 +48,38 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
      */
 
     // 날짜별 카테고리 지출금액 조회
-    @Query("select e.expenseDate, e.category, SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople)) else floor(e.amount) end) " +
-            "from Expense e JOIN e.accountBook ab WHERE ab.id = :accountBookId AND ab.member.id = :memberId GROUP BY e.expenseDate, e.category")
+    @Query("select e.expenseDate, e.category, " +
+            "SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople) * b.exchangeRate) else floor(e.amount * b.exchangeRate) end) " +
+            "from Expense e JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
+            "WHERE ab.id = :accountBookId AND ab.member.id = :memberId " +
+            "GROUP BY e.expenseDate, e.category")
     List<Object[]> findTotalAmountByDateAndCategory(@Param("accountBookId") Long accountBookId, @Param("memberId") Long memberId);
 
     // 날짜별 결제방법별 지출금액 조회
-    @Query("select e.paymentMethod, SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople)) else floor(e.amount) end) " +
-            "from Expense e JOIN e.accountBook ab WHERE ab.id = :accountBookId AND ab.member.id = :memberId AND e.expenseDate = :date GROUP BY e.paymentMethod")
+    @Query("select e.paymentMethod, " +
+            "SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople) * b.exchangeRate) else floor(e.amount * b.exchangeRate) end) " +
+            "from Expense e JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
+            "WHERE ab.id = :accountBookId AND ab.member.id = :memberId AND e.expenseDate = :date " +
+            "GROUP BY e.paymentMethod")
     List<Object[]> findTotalAmountByPaymentMethodAndDate(@Param("accountBookId") Long accountBookId, @Param("memberId") Long memberId, @Param("date") LocalDateTime date);
 
+
     // 카테고리별 총 지출금액 조회
-    @Query("select e.category, SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople)) else floor(e.amount) end) " +
-            "from Expense e JOIN e.accountBook ab WHERE ab.id = :accountBookId AND ab.member.id = :memberId GROUP BY e.category")
+    @Query("select e.category, " +
+            "SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople) * b.exchangeRate) else floor(e.amount * b.exchangeRate) end) " +
+            "from Expense e JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
+            "WHERE ab.id = :accountBookId AND ab.member.id = :memberId " +
+            "GROUP BY e.category")
     List<Object[]> findTotalAmountByCategoryForAll(@Param("accountBookId") Long accountBookId, @Param("memberId") Long memberId);
 
     /**
      * 예산 - 지출
      */
 
-// 특정 가계부의 총 지출금액 조회
-    @Query("SELECT SUM(CASE WHEN e.isShared = true THEN FLOOR(e.amount / ab.numberOfPeople) ELSE e.amount END) " +
+    // 특정 가계부의 총 지출금액 조회
+    @Query("SELECT SUM(CASE WHEN e.isShared = true THEN FLOOR((e.amount / ab.numberOfPeople) * b.exchangeRate) ELSE e.amount * b.exchangeRate END) " +
             "FROM Expense e " +
-            "JOIN e.accountBook ab " +
+            "JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
             "WHERE ab.id = :accountBookId")
     Double getTotalExpenseByAccountBookId(@Param("accountBookId") Long accountBookId);
 
@@ -77,14 +87,16 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
      * 라인차트 사실 라디오 버튼용임
      */
 
-// 날짜별 총 지출금액 조회
-    @Query("select e.expenseDate, SUM(case when e.isShared = true then floor(e.amount / ab.numberOfPeople) else floor(e.amount) end) " +
-            "from Expense e JOIN e.accountBook ab WHERE ab.id = :accountBookId AND ab.member.id = :memberId GROUP BY e.expenseDate")
+    // 날짜별 총 지출금액 조회
+    @Query("select e.expenseDate, SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople) * b.exchangeRate) else floor(e.amount * b.exchangeRate) end) " +
+            "from Expense e JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
+            "WHERE ab.id = :accountBookId AND ab.member.id = :memberId GROUP BY e.expenseDate")
     List<Object[]> findTotalAmountByDates(@Param("accountBookId") Long accountBookId, @Param("memberId") Long memberId);
 
     // 특정 카테고리의 날짜별 지출금액 조회
-    @Query("select e.expenseDate, e.category, SUM(case when e.isShared = true then floor(e.amount / ab.numberOfPeople) else floor(e.amount) end) " +
-            "from Expense e JOIN e.accountBook ab WHERE ab.id = :accountBookId AND ab.member.id = :memberId AND e.category in :categories GROUP BY e.expenseDate, e.category")
+    @Query("select e.expenseDate, e.category, SUM(case when e.isShared = true then floor((e.amount / ab.numberOfPeople) * b.exchangeRate) else floor(e.amount * b.exchangeRate) end) " +
+            "from Expense e JOIN e.accountBook ab JOIN Budget b ON b.accountBook.id = ab.id " +
+            "WHERE ab.id = :accountBookId AND ab.member.id = :memberId AND e.category in :categories GROUP BY e.expenseDate, e.category")
     List<Object[]> findTotalAmountByDatesAndCategories(@Param("accountBookId") Long accountBookId, @Param("memberId") Long memberId, @Param("categories") List<Category> categories);
 
 }
