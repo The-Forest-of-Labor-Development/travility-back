b8a13a55739fcbde2ea62305f660bd8c786b7264
heo5620 <tnqls1035@naver.com>
2024-07-31 11:07:27 +0900

    [fix] env 삭제 apiConfig로 통일

--- src/main/java/travility_back/travility/TravilityApplication.java
+++ src/main/java/travility_back/travility/TravilityApplication.java
@@ -2,12 +2,12 @@ package travility_back.travility;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravilityApplication {
+public class TravilityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravilityApplication.class, args);
 	}
-
 }
